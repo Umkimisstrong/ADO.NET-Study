ff90f562cd874192b60389e2f7090ba072f19240
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-01 01:23:58 +0900

    Sql Connection
    Sql Connection

---
+++ Test/adoNETStudy001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using adoNETStudy001;
+
+Console.WriteLine("Hello, World!");
+
+
+SQL_INFO info1 = new SQL_INFO("new Conneciton");
+SQL_INFO info2 = new SQL_INFO();
+
+Console.WriteLine(info1.sqlConnection);
+// new Connection
+Console.WriteLine(info2.sqlConnection);
+// Connection
+
+
---
+++ Test/adoNETStudy001/SQL_INFO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoNETStudy001
+{
+    public class SQL_INFO
+    {
+
+        public string sqlConnection = string.Empty;
+
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public SQL_INFO()
+        {
+            sqlConnection = "connection";
+        }
+
+
+        /// <summary>
+        /// 생성자(SQL 커넥션 지정)
+        /// </summary>
+        /// <param name="strSqlConnection">커넥션 문자열</param>
+        public SQL_INFO(string strSqlConnection)
+        {
+            this.sqlConnection = strSqlConnection;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection connection = null;
+
+            // getting SQL Connection use sqlConnection
+
+
+
+            return connection;
+        }
+
+    }
+}
